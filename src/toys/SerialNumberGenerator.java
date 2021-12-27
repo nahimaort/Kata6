d16c572b0ac844889ec725a7c6fcc4c4cb432806
nahimaort <nahimaortega@gmail.com>
2021-12-27 17:08:26 +0000

    Kata6 versión 1

---
+++ src/toys/SerialNumberGenerator.java
@@ -0,0 +1,11 @@
+package toys;
+
+public class SerialNumberGenerator {
+    private Integer serialNumber = 0;
+    
+    public Integer next() {
+        return this.serialNumber++;
+    }
+    
+    
+}
